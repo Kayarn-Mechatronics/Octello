6ab0079805ba8e9140e90cc1196c5a88eac11825
unknown <arnauldkayonga1@gmail.com>
2021-08-30 18:44:23 +0200

    Update

--- AuthenticationApp/models.py
+++ AuthenticationApp/models.py
@@ -7,3 +7,13 @@ from django.contrib.auth.models import AbstractUser
 #     class Meta:
 #         pass
 
+class Enterprises(models.Model):
+    enterprise_id = models.IntegerField(primary_key=True, null=False, unique=True)
+    name = models.CharField(max_length=100, null=False)
+    address = models.CharField(max_length=100, null=False)
+    country = models.CharField(max_length=100, null=False)
+    tin = models.CharField(max_length=100, null=False)
+    
+    
+    def __str__(self):
+        return self.account_name
